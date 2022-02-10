407c0ab4ee76aa893d909930d96ef16ad8625d4e
p.krysztofiak <p.krysztofiak@pixel.com.pl>
2022-02-10 07:13:27 +0100

    Minor refactor

--- src/main/java/pl/com/pixel/AuxView.java
+++ src/main/java/pl/com/pixel/AuxView.java
@@ -1,11 +1,22 @@
 package pl.com.pixel;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class AuxView extends StackPane {
 
     public AuxView() {
-        getChildren().add(new Label("Aux view"));
+        Button button = new Button("Click me!");
+        Label label = new Label();
+        VBox vBox = new VBox(button, label);
+        vBox.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+        StackPane stackPane = new StackPane(vBox);
+
+        button.setOnAction(event -> label.setText("Clicked"));
+
+        getChildren().add(stackPane);
     }
 }
